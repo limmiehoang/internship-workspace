aeecb691c50b6cf9c58cf5813e56cabbbaf8b279
limmiehoang <minhkhanhhoang2105@gmail.com>
2019-01-08 13:34:55 +0700

    fix(PHPDemo): sanitize output

--- PHP/views/inc/productRecord.php
+++ PHP/views/inc/productRecord.php
@@ -1,8 +1,12 @@
 <tr>
-    <td><?php echo $datum['id'] ?></td>
-    <td><?php echo $datum['product_name'] ?></td>
-    <td><?php echo $datum['username'] ?></td>
-    <td><?php echo $datum['category'] ?></td>
-    <td><?php echo $datum['group_name'] ?></td>
-    <td><a href="">View</a> <a href="/product/edit/<?php echo $datum['id']?>">Edit</a> <a href="">Remove</a> </td>
+    <td><?php echo htmlspecialchars($datum['id']) ?></td>
+    <td><?php echo htmlspecialchars($datum['product_name']) ?></td>
+    <td><?php echo htmlspecialchars($datum['username']) ?></td>
+    <td><?php echo htmlspecialchars($datum['category']) ?></td>
+    <td><?php echo htmlspecialchars($datum['group_name']) ?></td>
+    <td>
+        <a href="">View</a>
+        <a href="/product/edit/<?php echo $datum['id']?>">Edit</a>
+        <a class="del" href="/product/remove/<?php echo $datum['id']?>">Remove</a>
+    </td>
 </tr>
